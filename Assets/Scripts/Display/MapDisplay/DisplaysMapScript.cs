f1931b9ee9d6f4d8f0eadffea286f3cf4d9a00fb
Arkinion <nicholasgr44@gmail.com>
2018-10-17 12:32:59 -0400

    Set up all the files for the reorganization. The next step is full implementation

--- Assets/Scripts/Display/MapDisplay/DisplaysMapScript.cs
+++ Assets/Scripts/Display/MapDisplay/DisplaysMapScript.cs
@@ -2,12 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.Scripts.Display.MapToTexture;
+using Assets.Scripts.Generation.MapGeneration;
+using Assets.Scripts.Shared;
 using UnityEngine;
 
 namespace Assets.Scripts.Display.MapDisplay
 {
     public abstract class DisplaysMapScript : MonoBehaviour
     {
-        
+        public GameObject DisplaySurface;
+        public int MapHeight;
+        public int MapWidth;
+
+        private void Start()
+        {
+            GenerateNewMap();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                GenerateNewMap();
+            }
+        }
+
+        private void GenerateNewMap()
+        {
+            var mapGenerator = CreateMapGenerator();
+            var map = mapGenerator.GenerateMap(MapHeight, MapWidth);
+            DisplayMap(map);
+        }
+
+        private void DisplayMap(IMap map)
+        {
+            var mapToTextureTranslator = CreateMapToTextureTranslator();
+            DisplaySurface.GetComponent<MeshRenderer>().material.mainTexture =
+                mapToTextureTranslator.TranslateMapToTexture(map);
+        }
+
+        protected abstract IMapGenerator CreateMapGenerator();
+
+        protected abstract IMapToTextureTranslator CreateMapToTextureTranslator();
     }
 }
---
+++ Assets/Scripts/Display/MapDisplay/DisplaysMutationMarkov.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.Display.MapToTexture;
+using Assets.Scripts.Generation.MapGeneration;
+
+namespace Assets.Scripts.Display.MapDisplay
+{
+    public class DisplaysMutationMarkov : DisplaysMapScript
+    {
+        protected override IMapGenerator CreateMapGenerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IMapToTextureTranslator CreateMapToTextureTranslator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
