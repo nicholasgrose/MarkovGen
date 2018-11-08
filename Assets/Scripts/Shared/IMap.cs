1b609f86c9c1e573a2250a8cb3376a034d22885a
Arkinion <nicholasgr44@gmail.com>
2018-11-08 18:51:29 -0500

    Finished the IMap interface.
    The interface will probaby be split up later, but it is functional for now

--- Assets/Scripts/Shared/IMap.cs
+++ Assets/Scripts/Shared/IMap.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts.Shared
 {
     public interface IMap
     {
+        MapPixel[,] GetMap();
 
+        int Width();
+        int Height();
+
+        void SetPixelAt(Vector2Int coordinate, MapPixel value);
+        MapPixel GetPixelAt(Vector2Int coordinate);
+        MapPixel[] GetAdjacentPixels(Vector2Int coordinate);
     }
 }
